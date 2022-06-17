6170d2e8fd1fa2cf49bb35e600c61801a2352466
Gerilovich-Aleksey <you@example.com>
2022-06-17 10:37:17 +0300

    the button has been changed

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, TextInput, Button, Alert, Text} from 'react-native'
+import {View, StyleSheet, TextInput, Button, Alert, Image, TouchableOpacity} from 'react-native'
 
 export const AddTodo = ({onSubmit}) => {
 
@@ -20,11 +20,21 @@ export const AddTodo = ({onSubmit}) => {
 				style={styles.input}
 				onChangeText={setValue}
 				value={value}
-				placeholder="Введите текст"
+				placeholder="Введите текст!"
 			/>
-			<Button title='Добавить' onPress={pressHandler}/>
+	
+
+			<View style={styles.container}>
+				<TouchableOpacity style={styles.button} onPress={pressHandler}>
+					<Image source={require('../assets/airplane1.png')} style={styles.img}/>
+					
+				</TouchableOpacity>
+				
+			</View>
+
 		</View>
 	)
+
 }
 
 const styles = StyleSheet.create({
@@ -39,5 +49,27 @@ const styles = StyleSheet.create({
 		padding: 10,
 		borderStyle: 'solid',
 		borderBottomWidth: 2,
+	},
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	button: {
+		borderWidth:1,
+		borderColor:'rgba(0,0,0,0.2)',
+		alignItems:'center',
+		justifyContent:'center',
+		width:60,
+		height:60,
+		backgroundColor:'#fff',
+		borderRadius:50,
+		backgroundColor: '#DC143C',
+	},
+	img: {
+		height: 30,
+		width: 30,
+		resizeMode : 'stretch',
 	}
 })
